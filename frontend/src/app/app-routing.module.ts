95225de639c4401027a016967fd4fc9bc8fd9679
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-10-19 18:56:39 -0600

    Se creo componente para el registro de clientes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegistrarClienteComponent } from '../app/components/registrar-cliente/registrar-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'registrar_clase',
+    component: RegistrarClienteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
