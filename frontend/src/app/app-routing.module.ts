bf216d9f456ab05f3994317959ebb0b8bd15031d
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-10-26 17:39:01 -0600

    Se creo vista de inicio para encargado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioEncargadoComponent } from '../app/components/inicio-encargado/inicio-encargado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio_encargado',
+    component: InicioEncargadoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
